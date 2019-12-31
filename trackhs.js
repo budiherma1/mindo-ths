3eb01cf42b1f36d7e56c30cf7ed3399fdb3c8948
budianonym <budianonym@gmail.com>
2019-12-31 08:48:36 +0700

    2nd commit

---
+++ trackhs.js
@@ -0,0 +1,55 @@
+const axios = require('axios');
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+const theNow = Math.floor(Date.now() / 1000);
+const theUrl = `result-${theNow}.csv`;
+const csvWriter = createCsvWriter({
+    path: theUrl,
+    header: [
+        { id: 'url', title: 'URL' },
+        { id: 'sdescc', title: 'Short Desc' },
+        { id: 'ldesc', title: 'Long Desc' }
+    ]
+})
+
+ids = ['https://edencrest.trackhs.com/api/pms/units/91227','https://edencrest.trackhs.com/api/pms/units/91227']
+user = 'masukkan username dicini'
+pass = 'passwudnya di cini'
+
+records = []
+async function getData() {
+    for (id of ids){
+    let res = await axios.get(id, {
+        auth: {
+            username: user,
+            password: pass
+        },
+        headers: {
+            'Content-Type': 'applications/json'
+        }
+    });
+    // let sdesc = res.data;
+    // console.log(sdesc)
+    try {
+        records.push({
+            url: id,
+            sdescc: res.data.shortDescription,
+            ldesc: res.data.longDescription,
+        })
+        csvWriter.writeRecords(records)
+    } catch (error) {
+        records.push({
+            url: id,
+            sdescc: 'error',
+            ldesc: 'error',
+        })
+        csvWriter.writeRecords(records)
+    }
+
+    
+}
+}
+
+getData();
+
+
+
